c7a01b8919b18c6d331a35b5e800a5703a46aab4
br99bry <bryanblack1999@gmail.com>
2023-07-23 05:12:21 -0500

    fix: add types to layout

--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -1,9 +1,15 @@
-const Layout = ({ children }) => (
+import React, { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => (
   <div className="relative flex justify-center px-3 py-20 w-screen h-screen">
     <div className="w-default-app flex justify-center items-center flex-col	">
       {children}
     </div>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
